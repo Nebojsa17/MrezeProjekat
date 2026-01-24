ed2a2b96a40904a87e49b334c1a1df1a4784f6f9
Nebojsa <golusin.pr17.2023@uns.ac.rs>
2026-01-24 03:38:45 +0100

    Rename foldera servera

---
+++ Castle_Defense_Server/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonLibrary;
+
+namespace Castle_Defense_Server
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
