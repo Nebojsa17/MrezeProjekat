cae54475a490c005432cd3765fe5fb23fb67f4ae
Nebojsa <golusin.pr17.2023@uns.ac.rs>
2026-01-27 19:51:24 +0100

    Dodavanje potrebnih elemenata za prikaz

---
+++ Castle_Defense_Client/Elements/DrawingPanel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Castle_Defense_Client.Elements
+{
+    public partial class DrawingPanel : FrameworkElement
+    {
+        private VisualCollection visuals;
+
+        public DrawingPanel()
+        {
+            visuals = new VisualCollection(this);
+        }
+
+        public void AddVisual(DrawingVisual visual)
+        {
+            visuals.Add(visual);
+        }
+
+        public void Clear()
+        {
+            visuals.Clear();
+        }
+
+        protected override int VisualChildrenCount => visuals.Count;
+
+        protected override Visual GetVisualChild(int index)
+        {
+            return visuals[index];
+        }
+    }
+}
